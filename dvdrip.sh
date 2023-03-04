9444af25bb5baceca4e3e4eb60a43146cca2df22
TomcatMWI <tamas.polgar.pixeldog@gmail.com>
2023-03-05 00:03:24 +0100

    dvdrip.sh timer added

--- dvdrip.sh
+++ dvdrip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DVD="/dev/sr0"
-OUTDIR="/run/media/tomcatmi/Data/dvd"
+OUTDIR="/run/media/tomcatmwi/Data/dvd"
 
 # Check if the DVD path exists and is readable
 if [ ! -r "$DVD" ]; then
@@ -33,5 +33,21 @@ while true; do
   fi
 done
 
+# Save current time
+start_time=$(date +%s)
+echo "Starting DVD ripping at $(date +%H:%M:%S)"
+
 # Copy the contents of the DVD to the specified file in the OUTDIR directory
 dd if="$DVD" of="$OUTDIR/$filename" bs=2048 conv=sync,notrunc
+
+# Calculate the elapsed time
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+minutes=$((elapsed_time / 60))
+seconds=$((elapsed_time % 60))
+
+# Display the elapsed time
+echo "Ripping completed in $minutes minutes and $seconds seconds."
+echo "Space remaining on target drive: $(df -h $OUTDIR | awk '/dev/ {print $4}')"
+
+eject
